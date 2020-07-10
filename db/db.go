950ff92bed38e13a4bb7ac38aeebf15d2ac8a0db
Alex Shvets <shvetsiya@gmail.com>
2020-07-09 23:51:17 -0700

    updated web server and db

--- db/db.go
+++ db/db.go
@@ -1,8 +1,12 @@
 package db
 
 import (
+	"fmt"
+
 	bolt "go.etcd.io/bbolt"
 )
+
+var defaultBucket = []byte("default")
 
 // Database is an open bolt DB
 type Database struct {
@@ -15,6 +19,43 @@ func NewDatabase(dbPath string) (db *Database, closeFunc func() error, err error
 	if err != nil {
 		return nil, nil, err
 	}
+
+	db = &Database{db: boltDb}
 	closeFunc = boltDb.Close
-	return &Database{db: boltDb}, closeFunc, nil
+
+	if err := db.createBuckets(); err != nil {
+		closeFunc()
+		return nil, nil, fmt.Errorf("creating default bucket %w", err)
+	}
+	return db, closeFunc, nil
+}
+
+// SetKey ...
+func (d *Database) SetKey(key string, value []byte) error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(defaultBucket)
+		return b.Put([]byte(key), value)
+	})
+}
+
+// GetKey ...
+func (d *Database) GetKey(key string) ([]byte, error) {
+	var res []byte
+	err := d.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(defaultBucket)
+		res = b.Get([]byte(key))
+		return nil
+	})
+
+	if err == nil {
+		return res, nil
+	}
+	return nil, err
+}
+
+func (d *Database) createBuckets() error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(defaultBucket)
+		return err
+	})
 }
--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
+
+	"github.com/shvetsiya/distribkv/db"
 )
 
 var (
 	dbLocation = flag.String("db-location", "", "The path to bold db")
+	httpAddr = flag.String("http-addr", "127.0.0.1:8080", "http host and port")
 )
 
 func parseFlags() {
@@ -20,4 +25,27 @@ func parseFlags() {
 func main() {
 	parseFlags()
 
+	db, close, err := db.NewDatabase(*dbLocation)
+	if err != nil {
+		log.Fatalf("NewDatabase(%q): v", *dbLocation, err)
+	}
+	defer close()
+
+	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		key := r.Form.Get("key")
+		value, err := db.GetKey(key)
+		fmt.Fprintf(w, "Value = %q, err = %v\n", value, err)
+	})
+
+
+	http.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		key := r.Form.Get("key")
+		value := r.Form.Get("value")
+		err := db.SetKey(key, []byte(value))
+		fmt.Fprintf(w, "Error = %v\n", err)
+	})
+
+	log.Fatal(http.ListenAndServe(*httpAddr, nil))
 }
